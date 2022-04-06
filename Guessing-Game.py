d9b3f006dc4cf91a870f9046c5abe0b76cc05ba1
Jared Mindel <Jmindel@Dev-10.com>
2022-04-06 10:16:57 -0700

    Guessing-Game.py

--- Guessing-Game.py
+++ Guessing-Game.py
@@ -1,13 +1,29 @@
-### Guessing Game ###
+##### Part 2 #####
 
-guess = "Thanos"
+word = "Google"
+guesses_left = 7
 
-def guessing_game(guess):
-  if len(guess) = 1:
-    guess_type = "letter"
-  else: 
-    guess_type = "word"
-  if guess_type == "letter"
-  
-  
-#if they have more than 2 guesses left#
+while guesses_left > 0:
+    guess = input("Guess the word")
+    if len(guess) == 1: # The guess is a letter here
+        if guess in word:
+            if word.count(guess) == 1:
+                print(word.index(guess) + 1) #show the location of the letter; I added 1 because Python indexes at 0
+            else:
+                for letter in word:
+                    if guess == letter:
+                        print(word[letter])
+        else:
+            print("That letter is not in the word")
+            guesses_left = guesses_left - 1
+            print("you have " + str(guesses_left) + " guesses left.")
+    else: # The guess is a full word
+        if guess == word:
+            print("Hooray!")
+            break
+        else:
+            print("Incorrect")
+            guesses_left = guesses_left - 1
+            print("you have " + str(guesses_left) + " guesses left.")
+
+print("Hey")
