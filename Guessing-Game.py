77ebd0bc7c1b65106690ded48b151199bbca5a10
Jared Mindel <Jmindel@Dev-10.com>
2022-04-06 12:37:24 -0700

    Guessing-Game.py

--- Guessing-Game.py
+++ Guessing-Game.py
@@ -6,19 +6,20 @@ new_word = ""
 guesses_left = 7
 
 for quantity in word:
-    listed_word[quantity] = "_"
+    listed_word = listed_word + "_"
 
 while guesses_left > 0:
     guess = input("Make a guess").lower()
     if len(guess) == 1: # The guess is a letter here
         if guess in word:
-            if word.count(guess) == 1:
-                print(word.index(guess) + 1) #show the location of the letter; I added 1 because Python indexes at 0
+            for letter_spot in range(len(word)):
+                if(word[letter_spot] == guess):
+                    listed_word = list(listed_word)
+                    listed_word[letter_spot] = word[letter_spot]
+            if listed_word == list(word):
+                print('Hooray! You Won!')
+                break
             else:
-                for letter_spot in range(len(word)):
-                    if(word[letter_spot] == guess):
-                        listed_word[letter_spot] = guess
-                    else: listed_word[letter_spot] = "_"
                 print(" ".join(listed_word))
         else:
             print("That letter is not in the word")
@@ -26,7 +27,7 @@ while guesses_left > 0:
             print("you have " + str(guesses_left) + " guesses left.") #show how many guesses are left
     else: # The guess is a full word here
         if guess == word:
-            print("Hooray!")
+            print("Hooray! You Won!")
             break
         else:
             print("Incorrect")
