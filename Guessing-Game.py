7d6316227e2245755fe5bbdaad6a32aeec24d6de
Jared Mindel <Jmindel@Dev-10.com>
2022-04-07 12:31:18 -0700

    --all

--- Guessing-Game.py
+++ Guessing-Game.py
@@ -1,9 +1,5 @@
-import os
 import json
 import random
-
-os.chdir(r'C:\Users\jared\Dev-10 Module Exercises\Python\Python Basics\Guessing Game')
-print(os.getcwd())
 
 words_dictionary = open(r'words_dictionary.json')
 dictionary_lines = words_dictionary.read().splitlines()
